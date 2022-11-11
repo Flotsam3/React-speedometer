f2bb13079780291c6918b481991aba3315e91d8f
Joker <admin@e.mail.de>
2022-11-11 01:45:33 +0100

    Completed without sound

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,5 +1,42 @@
-import React from "react";
+import {React,useReducer} from "react";
+import ReactSpeedometer from "react-d3-speedometer";
+import Button from "./Button";
+
+const reduce = (state, action) => {
+  switch (action.type) {
+    case "on/off":
+        if (state.motor === "off"){
+          return {...state, motor: "on"}
+        }
+        return {...state, motor: "off", speed: 0}
+      case "accelerate":
+        if (state.motor === "on" && state.speed <= 280){
+          return {...state, speed: state.speed + 5}
+        }
+        return state
+      case "brake":
+        if (state.motor === "on" && state.speed >= 5){
+          return {...state, speed: state.speed - 5}
+        }
+        return state
+    default:
+      return state
+  }
+}
 
 export default function Car() {
-  return <div className="car">Make your instrument cluster here</div>;
+  const [state, dispatch] = useReducer(reduce, {motor: "off", speed: 0})
+  console.log(state);
+
+  
+
+  return <div className="car">
+    {state.motor === "on" ? <ReactSpeedometer startColor="green" endColor="red" value={state.speed} currentValueText={state.speed + "km/h"} maxValue={280} customSegmentStops={[0, 50, 100, 130, 180, 230, 280]}/>: <h2>Ausgeschaltet</h2>}
+    
+    <div className="buttonWrapper">
+      <Button value={state.motor === "on" ? "Ausschalten" : "Starten"} dispatch={dispatch} type={"on/off"}/>
+      <Button value={"Gas geben"} dispatch={dispatch} type={"accelerate"}/>
+      <Button value={"Bremsen"} dispatch={dispatch} type={"brake"}/>
+    </div>
+  </div>;
 }
